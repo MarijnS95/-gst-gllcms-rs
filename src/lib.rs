MarijnS95/gst-gllcms-rs

Deny all default-allow lints, except insensible ones
Many default-allow lints enforce appropriate rules but are disabled
because they are either too opinionated or report severe known
false-positives. As a small POC project in itself this is the right
place to simply enable them all and selectively disable those that are
nonsenical, rather than trying to find and enable an (ever-expanding)
list of extra lints.

src/lib.rs
@@ -1,57 +1,21 @@
-// BEGIN - Embark clippy lints v0.3: https://github.com/EmbarkStudios/rust-ecosystem/blob/c8e9ce2e9b61195d84511de08354694e2df2afae/lints.rs
 #![warn(
-    clippy::all,
-    clippy::await_holding_lock,
-    clippy::dbg_macro,
-    clippy::debug_assert_with_mut_call,
-    clippy::doc_markdown,
-    clippy::empty_enum,
-    clippy::enum_glob_use,
-    clippy::exit,
-    clippy::explicit_into_iter_loop,
-    clippy::filter_map_next,
-    clippy::fn_params_excessive_bools,
-    clippy::if_let_mutex,
-    clippy::imprecise_flops,
-    clippy::inefficient_to_string,
-    clippy::large_types_passed_by_value,
-    clippy::let_unit_value,
-    clippy::linkedlist,
-    clippy::lossy_float_literal,
-    clippy::macro_use_imports,
-    clippy::map_err_ignore,
-    clippy::map_flatten,
-    clippy::map_unwrap_or,
-    clippy::match_on_vec_items,
-    clippy::match_same_arms,
-    clippy::match_wildcard_for_single_variants,
-    clippy::mem_forget,
-    clippy::mismatched_target_os,
-    clippy::needless_borrow,
-    clippy::needless_continue,
-    clippy::option_option,
-    clippy::pub_enum_variant_names,
-    clippy::ref_option_ref,
-    clippy::rest_pat_in_fully_bound_structs,
-    clippy::string_add_assign,
-    clippy::string_add,
-    clippy::string_to_string,
-    clippy::suboptimal_flops,
-    clippy::todo,
-    clippy::unimplemented,
-    clippy::unnested_or_patterns,
-    clippy::unused_self,
-    clippy::verbose_file_reads,
-    future_incompatible,
+    clippy::nursery,
+    clippy::pedantic,
+    // clippy::cargo,
+    // clippy::restriction,
     nonstandard_style,
-    rust_2018_idioms
+    rust_2018_idioms,
+    rust_2018_compatibility,
 )]
-// Local exceptions
-#![allow(clippy::unimplemented, clippy::todo)]
-// END - Embark clippy lints
-
-// Custom extra lints
-#![warn(clippy::single_match_else, clippy::clippy::ptr_as_ptr)]
+#![allow(
+    clippy::default_trait_access,
+    clippy::shadow_unrelated,
+    clippy::too_many_lines,
+    clippy::unseparated_literal_suffix,
+    clippy::wildcard_imports,
+    clippy::unimplemented,
+    clippy::todo
+)]
 
 use gst::glib;
 use gst::subclass::prelude::*;

MarijnS95/-gst-gllcms-rs

src/lib.rs
@@ -1,57 +1,22 @@
-// BEGIN - Embark clippy lints v0.3: https://github.com/EmbarkStudios/rust-ecosystem/blob/c8e9ce2e9b61195d84511de08354694e2df2afae/lints.rs
 #![warn(
-    clippy::all,
-    clippy::await_holding_lock,
-    clippy::dbg_macro,
-    clippy::debug_assert_with_mut_call,
-    clippy::doc_markdown,
-    clippy::empty_enum,
-    clippy::enum_glob_use,
-    clippy::exit,
-    clippy::explicit_into_iter_loop,
-    clippy::filter_map_next,
-    clippy::fn_params_excessive_bools,
-    clippy::if_let_mutex,
-    clippy::imprecise_flops,
-    clippy::inefficient_to_string,
-    clippy::large_types_passed_by_value,
-    clippy::let_unit_value,
-    clippy::linkedlist,
-    clippy::lossy_float_literal,
-    clippy::macro_use_imports,
-    clippy::map_err_ignore,
-    clippy::map_flatten,
-    clippy::map_unwrap_or,
-    clippy::match_on_vec_items,
-    clippy::match_same_arms,
-    clippy::match_wildcard_for_single_variants,
-    clippy::mem_forget,
-    clippy::mismatched_target_os,
-    clippy::needless_borrow,
-    clippy::needless_continue,
-    clippy::option_option,
-    clippy::pub_enum_variant_names,
-    clippy::ref_option_ref,
-    clippy::rest_pat_in_fully_bound_structs,
-    clippy::string_add_assign,
-    clippy::string_add,
-    clippy::string_to_string,
-    clippy::suboptimal_flops,
-    clippy::todo,
-    clippy::unimplemented,
-    clippy::unnested_or_patterns,
-    clippy::unused_self,
-    clippy::verbose_file_reads,
-    future_incompatible,
+    clippy::nursery,
+    clippy::pedantic,
+    // clippy::cargo,
+    // clippy::restriction,
     nonstandard_style,
-    rust_2018_idioms
+    rust_2018_idioms,
+    rust_2018_compatibility,
 )]
-// Local exceptions
-#![allow(clippy::unimplemented, clippy::todo)]
-// END - Embark clippy lints
-
-// Custom extra lints
-#![warn(clippy::single_match_else, clippy::clippy::ptr_as_ptr)]
+#![allow(
+    clippy::default_trait_access,
+    clippy::semicolon_if_nothing_returned,
+    clippy::shadow_unrelated,
+    clippy::todo,
+    clippy::too_many_lines,
+    clippy::unimplemented,
+    clippy::unseparated_literal_suffix,
+    clippy::wildcard_imports
+)]
 
 use gst::glib;
 use gst::subclass::prelude::*;